77b87210d49ce769d495bea9a51fb6b97d115b2d
gelos <9svtcrze9b2m@mail.ru>
2019-01-14 15:15:25 +0300

    Replaced Feign to bowman. Bowman  client that support JSON+HAL.

--- src/main/java/com/example/demo/DepartmentAppWebApplication.java
+++ src/main/java/com/example/demo/DepartmentAppWebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @SpringBootApplication
 public class DepartmentAppWebApplication {
 
